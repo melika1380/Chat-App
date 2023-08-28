b1407ff7a8ee4d2846c5c5dbdf95331c3cf69276
unknown <melikamoslehoffical@gmail.com>
2023-08-28 19:50:58 +0330

    css . html

---
+++ src/main/index.js
@@ -0,0 +1,10 @@
+import html from './index.html'
+import "./index.scss";
+
+
+function loaderPage() {
+    const li=document.querySelector(".shopping-list li");
+   
+}
+window.addEventListener("load", loaderPage);
+export default html;
