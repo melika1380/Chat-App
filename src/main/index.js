a60753111f7177efa13c9f9c060801ecb748a2cd
unknown <melikamoslehoffical@gmail.com>
2023-09-02 22:17:47 +0330

    edit

--- src/main/index.js
+++ src/main/index.js
@@ -3,44 +3,48 @@ import "./index.scss";
 import { getName } from "../header/index";
 
 function loaderPage() {
-
-  const socket = new WebSocket("wss://ws.postman-echo.com/raw");
-  const btnInput = document.querySelector(".button");
-  const userInput = document.querySelector(".input");
-
-  function statueSocket(event) {
-    console.log("connecting", event);
-  }
-  socket.addEventListener("open", statueSocket);
-
-  function messageSocket(event) {
-    const messagesContainer = document.querySelector(".one");
-    const messageElement = document.createElement("li");
-    messageElement.textContent = event.data;
-    messagesContainer.appendChild(messageElement);
-  }
-  socket.addEventListener("message", messageSocket);
-
-  function sendMessage() {
+  try{
+    const socket = new WebSocket("wss://ws.postman-echo.com/raw");
+    const btnInput = document.querySelector(".button");
     const userInput = document.querySelector(".input");
-    const message = userInput.value;
-    if (message.trim() !== "") {
-      socket.send(message);
+    getName();
+    function statueSocket(event) {
+      console.log("connecting", event);
+    }
+    socket.addEventListener("open", statueSocket);
+  
+    function messageSocket(event) {
       const messagesContainer = document.querySelector(".two");
-      const userMessageElement = document.createElement("li");
-      userMessageElement.textContent = message;
-      messagesContainer.appendChild(userMessageElement);
-      userInput.value = "";
+      const messageElement = document.createElement("li");
+      messageElement.textContent = event.data;
+      messagesContainer.appendChild(messageElement);
     }
-  }
-  btnInput.addEventListener("click", sendMessage);
-
-  function enterClick(event) {
-    if (event.key === "Enter") {
-      sendMessage();
+    socket.addEventListener("message", messageSocket);
+  
+    function sendMessage() {
+      const userInput = document.querySelector(".input");
+      const message = userInput.value;
+      if (message.trim() !== "") {
+        socket.send(message);
+        const messagesContainer = document.querySelector(".one");
+        const userMessageElement = document.createElement("li");
+        userMessageElement.textContent = message;
+        messagesContainer.appendChild(userMessageElement);
+        userInput.value = "";
+      }
     }
+    btnInput.addEventListener("click", sendMessage);
+  
+    function enterClick(event) {
+      if (event.key === "Enter") {
+        sendMessage();
+      }
+    }
+    userInput.addEventListener("keyup", enterClick);
+  }catch{
+    alert("loading");
   }
-  userInput.addEventListener("keyup", enterClick);
+ 
 }
 
 window.addEventListener("load", loaderPage);
